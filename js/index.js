d402b6a31d7531499c37079e1aa5b417d63c9aaa
Rodolfo <rodormz98@gmail.com>
2020-11-30 12:04:24 -0600

    Added categories functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const gamesUrl = 'https://tiendajuegos-92ebb.firebaseio.com/games.json'
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Get all "navbar-burger" elements
@@ -24,11 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
   
   });
 
-  fetch('https://tiendajuegos-92ebb.firebaseio.com/games.json')
+  function fetchData() {
+  let productsColumn = document.querySelector('.productsColumns')
+  let featuredColumns = document.querySelector('.featuredColumn')
+  fetch(gamesUrl)
   .then((response) => {
     return response.json()
   })
   .then((data) => {
+    
     for(i in data) {
       let name = data[i].name;
       let platform = data[i].platform;
@@ -36,8 +42,6 @@ document.addEventListener('DOMContentLoaded', () => {
       let category = data[i].category;
       let desc = data[i].desc;
       let imgUrl = data[i].imgUrl;
-      let productsColumn = document.querySelector('.productsColumns')
-      let featuredColumns = document.querySelector('.featuredColumn')
       let newCard = `
       <div class="column is-one-fourth">
       <div class="card">
@@ -75,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
     </div>
     </div>`
     productsColumn.innerHTML = productsColumn.innerHTML + newCard;
-
+    
     if(data[i].featured) {
       let featuredItem = `<div class="column">
       <div class="card">
@@ -121,3 +125,116 @@ document.addEventListener('DOMContentLoaded', () => {
   .catch((err) => {
     // Do something for an error here
   })
+}
+
+function fetchCategories() {
+  let productsColumn = document.querySelector('.productsColumns')
+  fetch(gamesUrl)
+  .then ((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    const items = document.querySelectorAll('.categories-item');
+    items.forEach(item => {
+	  item.addEventListener('click',(e)=>{
+      productsColumn.innerHTML =""
+		for(i in data) {
+      if (item.textContent == data[i].category) {
+        let name = data[i].name;
+        let platform = data[i].platform;
+        let price = data[i].price;
+        let desc = data[i].desc;
+        let imgUrl = data[i].imgUrl;
+        let newCard = `
+      <div class="column is-one-fourth">
+      <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="${imgUrl}" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+          </div>
+          <div class="media-content">
+            <p class="title is-4">${name}</p>
+            <div class="row">
+              <div class="columns">
+                <div class="column">
+                  <p class="subtitle is-6;" style="font-weight:bold">${platform}</p>
+                </div>
+                <div class="column">
+                  <p class="subtitle is-6 has-text-right" style="padding-right:2em; font-weight: bold;">$${price}</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="content">
+          <p>${desc}</p>
+          <br>
+          <br>
+          <a class="button is-black is-outlined"><img src="./assets/icons/shopping_cart.svg" style="width: 20px; height: 20px;margin-left:8px;margin-right:5px;" alt="" class="src">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+    </div>`
+        productsColumn.innerHTML = productsColumn.innerHTML + newCard;
+      } else if (item.textContent == 'Todos') {
+        productsColumn.innerHTML = ""
+        for(i in data) {
+          let name = data[i].name;
+        let platform = data[i].platform;
+        let price = data[i].price;
+        let desc = data[i].desc;
+        let imgUrl = data[i].imgUrl;
+        let newCard = `
+      <div class="column is-one-fourth">
+      <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="${imgUrl}" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+          </div>
+          <div class="media-content">
+            <p class="title is-4">${name}</p>
+            <div class="row">
+              <div class="columns">
+                <div class="column">
+                  <p class="subtitle is-6;" style="font-weight:bold">${platform}</p>
+                </div>
+                <div class="column">
+                  <p class="subtitle is-6 has-text-right" style="padding-right:2em; font-weight: bold;">$${price}</p>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="content">
+          <p>${desc}</p>
+          <br>
+          <br>
+          <a class="button is-black is-outlined"><img src="./assets/icons/shopping_cart.svg" style="width: 20px; height: 20px;margin-left:8px;margin-right:5px;" alt="" class="src">Agregar al carrito</a>
+        </div>
+      </div>
+    </div>
+    </div>`
+        productsColumn.innerHTML = productsColumn.innerHTML + newCard;
+        }
+      }
+    }
+	}
+	)
+})
+  })
+  .catch((err) => {
+
+  })
+}
