0ce8ea3d88288d6cd3219346f519352dcb17e347
Rodolfo <rodormz98@gmail.com>
2020-11-30 21:16:28 -0600

    Fixed auth issues

--- js/firebaseAuth.js
+++ js/firebaseAuth.js
@@ -3,35 +3,37 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       //Usuario con sesion iniciada
-      let cartIcon = document.querySelector('#cartIcon')
-      let userIcon = document.querySelector('#userIcon')
-      let btnCheckout = document.querySelector('.btnCheckout')
-      let btnPay = document.querySelector('.btnPay')
-      cartIcon.href = '../cart.html'
-      btnPay.href = '../inventory.html'
-      userIcon.href="../inventory.html"
-      btnCheckout.href = '../checkout.html'
       addToCart(user);
       showCart(user.uid);
       checkOut(user.uid)
       giveKeys(user.uid)
-    
-      userIcon.innerText = "Cerrar sesion"
+      let closeSession = document.querySelector('.closeSession')
+      let cartIcon = document.querySelector('#cartIcon')
+      let userIcon = document.querySelector('#userIcon')
+      let btnCheckout = document.querySelector('.btnCheckout')
+      let btnInventory = document.querySelector('.btnPay')
+      btnInventory.href="./inventory.html"
+      btnCheckout.href = '../checkout.html'
+      cartIcon.href = '../cart.html'
+      closeSession.style.display = "block"
+      userIcon.href="../inventory.html"
+      
+      // userIcon.innerText = "Cerrar sesion"
       userIcon.style.color = "white"
-      userIcon.addEventListener('click' , () => {
+      closeSession.addEventListener('click' , () => {
         firebase.auth().signOut().then(function() {
           window.location = '../index.html'
+          closeSession.style.display = "none"
           userIcon.href = "../login.html"
           userIcon.innerHTML = '<img src="./assets/icons/account.svg" style="padding-right: 2em;"/>'
         }).catch(function(error) {
           // An error happened.
         });
       })
-      // userIcon.style.textDecoration = "none"
-      // let btnCheckout = document.querySelector('.btnCheckout')
-      // btnCheckout.href = '../checkout.html'
+      userIcon.style.textDecoration = "none"
     } else {
       cartIcon.href = "../login.html"
+      userIcon.href = "../login.html"
       console.log("User not logged in")
     }
   });
@@ -93,6 +95,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   }
 
   function checkOut(user) {
+    
     var ref = firebase.database().ref("users").child(user);
     let total = 0
     let checkOutItemsGames = document.querySelector('.juego')
